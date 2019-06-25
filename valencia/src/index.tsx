6480d22b8e17ec8f16fa0b1d86f3c9e5165ef809
Elizabeth Alexander <eealexander@cs.uchicago.edu>
2019-06-25 16:38:03 -0700

    Added routes

---
+++ valencia/src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Routes } from "./routes";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+
+ReactDOM.render(
+  <Routes />,
+  document.getElementById("root") as HTMLElement
+);
+registerServiceWorker();
